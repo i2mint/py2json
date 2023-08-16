9132361a7e9dcdd9806458369d0f84e880a77567
Andie.Phan <andie.phan@analog.com>
2023-08-16 16:19:52 -0700

    add docs to Ctor init

--- py2json/ctor.py
+++ py2json/ctor.py
@@ -346,7 +346,14 @@ class Ctor(CtorNames):
 
     _deconstruction_specs = None
 
-    def __init__(self, deconstruction_specs=None):
+    def __init__(self, deconstruction_specs: List[dict, callable]=None):
+        """Create a new Ctor instance with custom deconstruction specs
+
+        :param deconstruction_specs: Can be a list of dicts or callables.
+            See numpy_deconstruction_spec for an example of a callable spec that returns a dict spec.
+        :type deconstruction_specs: List[dict, callable], optional
+        """
+
         if deconstruction_specs:
             self._deconstruction_specs = deconstruction_specs
 
